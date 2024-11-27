996dd1d0d238e2d9f7fe73363f417572b9038efa
Yılmaz Ev <yilmaz.ev@outlook.com>
2024-11-28 01:28:50 +0300

    port added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,18 +4,25 @@ import (
 	"golang-todo-app/config"
 	"golang-todo-app/internal/http"
 	"log"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	config.LoadEnv()
+
 	db := config.ConnectDB()
 	defer db.Close()
 
 	e := echo.New()
 	http.RegisterRoutes(e, db)
 
-	log.Println("Starting server on port 8080...")
-	e.Logger.Fatal(e.Start(":8080"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Starting server on " + port)
+	e.Logger.Fatal(e.Start(":" + port))
 }
--- internal/domain/models/todo.go
+++ internal/domain/models/todo.go
@@ -1,7 +1,7 @@
 package models
 
 type Todo struct {
-	ID          int    `json:"id"`
+	ID          uint   `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Completed   bool   `json:"completed"`
